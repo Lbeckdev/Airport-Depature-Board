1716efc4b14b1df5f829233c736e928aa7ea4aa7
Lukas-Beck <126213947+Lbeckdev@users.noreply.github.com>
2024-05-01 11:20:48 +0200

    Configure initial routes
    Configured initial route to departure-board component with redirect from "empty URL" (full path match).

--- Departure-Board/src/app/app.routes.ts
+++ Departure-Board/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DepartureBoardComponent } from './departure-board/departure-board.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/departure-board', pathMatch: 'full' },
+  { path: 'departure-board', component: DepartureBoardComponent }
+];
