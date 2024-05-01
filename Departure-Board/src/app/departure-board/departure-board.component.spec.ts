02bae913491978eacddf4ab12f46d4975d13b546
Lukas-Beck <126213947+Lbeckdev@users.noreply.github.com>
2024-05-01 10:55:37 +0200

    Create departure-board component

---
+++ Departure-Board/src/app/departure-board/departure-board.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DepartureBoardComponent } from './departure-board.component';
+
+describe('DepartureBoardComponent', () => {
+  let component: DepartureBoardComponent;
+  let fixture: ComponentFixture<DepartureBoardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DepartureBoardComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(DepartureBoardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Departure-Board/src/app/departure-board/departure-board.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-departure-board',
+  standalone: true,
+  imports: [],
+  templateUrl: './departure-board.component.html',
+  styleUrl: './departure-board.component.css'
+})
+export class DepartureBoardComponent {
+
+}
